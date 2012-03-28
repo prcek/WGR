ce5a0f19030f1711724b3d44dfc0b99c4f9a4b63
Tomas Hluchan <tomas.hluchan@gmail.com>
2012-03-28 21:28:56 +0200

    post

--- wgr.py
+++ wgr.py
@@ -4,6 +4,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 import os
 from google.appengine.ext.webapp import template
+import logging
 
 class Gift(db.Model):
     name = db.StringProperty(required=True)
@@ -13,33 +14,33 @@ class Gift(db.Model):
 
 
 class MainPage(webapp.RequestHandler):
+    def post(self):
+	logging.info('post')
+	gift = Gift(url='xxx',name='tddd')
+	gift.url='xxx'	
+	gift.name='tsss'
+	gift.put()
+	self.redirect('/')
+
+
     def get(self):
 
         user = users.get_current_user()
 
+	gifts = Gift.all().order('order_value')
 
-        self.response.headers['Content-Type'] = 'text/plain'
-	gifts = Gift.all()
-	for gift in gifts:
-	    self.response.out.write('gift '+gift.desc) 
-	    self.response.out.write(' ')
-
-        if user:
-            self.response.out.write('Hello, webapp World! '+user.nickname())
-        else:
-#            self.redirect(users.create_login_url(self.request.uri))
-            self.response.out.write('Hello, webapp World!')
 
 	template_values = {
-		'val': 'test'
+		'user':user,
+		'gifts': gifts
 	}
 	path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
 	
 
-application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
-                                     debug=True)
+application = webapp.WSGIApplication( 
+				     [ ('/', MainPage) ]
+				     , debug=True)
 
 def main():
     run_wsgi_app(application)
